3dc3ee6d0fd9d18861dbb0d1b0a49e94b51a847e
Joe Greiner <jrgreiner115@gmail.com>
2018-03-22 02:44:34 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = [fender, strymon, empress, gretsch]
+  array = ["fender", "strymon", "empress", "gretsch"]
 end
 
 def add_element_to_end_of_array(array, element)
